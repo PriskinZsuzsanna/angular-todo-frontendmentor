3fd1a1649c70ca74282b422a0059c3428d17cee3
Priskin Zsuzsanna <priskin.zsuzsanna@gmail.com>
2023-05-31 15:42:15 +0200

    working3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,19 @@ import { Todo } from './todo';
 })
 export class AppComponent {
   title = 'angular-todo-frontendmentor';
+  localTodos: Array<Todo> = new Array<Todo>
   todos: Array<Todo> = new Array<Todo>
   actual:Todo = new Todo ()
+  active:  Array<Todo> = new Array<Todo>
+  completed:  Array<Todo> = new Array<Todo>
+  left: number = 0;
+  isFilterAll:boolean = true
+  isFilterActive:boolean = false
+  isFilterCompleted:boolean = false
 
   constructor(){
     this.load()
+    this.sortCompleted()
   }
 
   add(){
@@ -23,11 +31,45 @@ export class AppComponent {
   }
 
   load(){
-    this.todos = JSON.parse(localStorage.getItem("todos") ?? '[]')
-    console.log(this.todos)
+    this.localTodos = JSON.parse(localStorage.getItem("todos") ?? '[]')
+    this.todos = this.localTodos
   }
 
   save(){
     localStorage.setItem("todos", JSON.stringify(this.todos))
   }
+
+  sortCompleted(){
+    this.active = this.todos.filter(todo => {
+      return todo.completed == false
+    })
+    this.completed= this.todos.filter(todo => {
+      return todo.completed == true
+    })
+    this.left = this.active.length
+  }
+
+  getFilter(filter:string){
+    this.isFilterActive = false;
+    this.isFilterAll = false;
+    this.isFilterCompleted = false;
+
+    if(filter == "completed"){
+      this.todos = this.completed
+      this.isFilterCompleted = true
+    } else if (filter == "active"){
+      this.todos = this.active
+      this.isFilterActive = true
+    } else {
+      this.todos = this.localTodos
+      this.isFilterAll = true
+    }
+  }
+
+  toggleComplete(todo:Todo){
+    console.log(todo.completed)
+    todo.completed = !todo.completed
+
+  }
+
 }
